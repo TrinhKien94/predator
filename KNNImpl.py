36beffff099547782cf9c9d5aafce2d9b27a1316
TrinhKien94 <TrinhKien94@users.noreply.github.com>
2019-08-05 11:37:26 +0700

    get accuracy for multi predict

--- KNNImpl.py
+++ KNNImpl.py
@@ -49,7 +49,13 @@ def getAccuracy(testSet, predictions):
         if testSet[x][-1] == predictions[x]:
             correct += 1
     return (correct/float(len(testSet))) * 100.0
-	
+
+def getAccuracyForMultiPredict(testSet, predictions):
+    correct = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] in predictions[x]:
+            correct += 1
+    return (correct/float(len(testSet))) * 100.0
 def main():
     # prepare data
     trainingSet=[]
